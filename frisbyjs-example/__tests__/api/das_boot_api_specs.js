e99642449a299e757482222cf576764e82f6d983
Unmesh Gundecha <upgundecha@gmail.com>
2018-06-03 23:20:04 +0530

    Tests for POST API

--- frisbyjs-example/__tests__/api/das_boot_api_specs.js
+++ frisbyjs-example/__tests__/api/das_boot_api_specs.js
@@ -4,3 +4,17 @@ it('should get list of shipwrecks', function() {
     return frisby.get('http://localhost:9999/api/v1/shipwrecks')
         .expect('status', 200);
 });
+
+it ('should create a shipwrecks record', function () {
+    return frisby
+      .post('http://localhost:9999/api/v1/shipwrecks', {
+        name: "U Boat 66",
+        description: "German U Boat 66",
+        condition: "Fair",
+        yearDiscovered: "2000",
+        depth: "1000",
+        latitude: "49.395203",
+        longitude: "-37.302391"
+      })
+      .expect('status', 200)
+  });
