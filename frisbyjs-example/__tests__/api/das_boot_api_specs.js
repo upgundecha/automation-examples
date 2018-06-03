28201523f426a68a5b6e8d533666aebf4fa0ad1a
Unmesh Gundecha <upgundecha@gmail.com>
2018-06-03 23:33:50 +0530

    Nested test

--- frisbyjs-example/__tests__/api/das_boot_api_specs.js
+++ frisbyjs-example/__tests__/api/das_boot_api_specs.js
@@ -1,5 +1,15 @@
 const frisby = require('frisby')
 
+let ship = {
+    name: "U Boat 66",
+    description: "German U Boat 66",
+    condition: "Fair",
+    yearDiscovered: "2000",
+    depth: "1000",
+    latitude: "49.395203",
+    longitude: "-37.302391"
+}
+
 it('should get list of shipwrecks', function() {
     return frisby.get('http://localhost:9999/api/v1/shipwrecks')
         .expect('status', 200);
@@ -7,19 +17,18 @@ it('should get list of shipwrecks', function() {
 
 it ('should create a shipwrecks record', function () {
     return frisby
-      .post('http://localhost:9999/api/v1/shipwrecks', {
-        name: "U Boat 66",
-        description: "German U Boat 66",
-        condition: "Fair",
-        yearDiscovered: "2000",
-        depth: "1000",
-        latitude: "49.395203",
-        longitude: "-37.302391"
-      })
+      .post('http://localhost:9999/api/v1/shipwrecks', ship)
       .expect('status', 200)
       .then(function (res){
           let id = res.json.id;
          return frisby.get(`http://localhost:9999/api/v1/shipwrecks/${id}`)
-          .expect('status', 200);
+          .expect('status', 200)
+          .expect('json', 'name', ship.name)
+          .expect('json', 'description', ship.description)
+          .expect('json', 'condition', ship.condition)
+          .expect('json', 'yearDiscovered', ship.yearDiscovered)
+          .expect('json', 'depth', ship.depth)
+          .expect('json', 'latitude', ship.latitude)    
+          .expect('json', 'longitude', ship.longitude)
       })
   });
